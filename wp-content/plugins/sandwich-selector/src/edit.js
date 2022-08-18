092d53cb41ea7cd0ca3217d1d5c19c1005104f2a
Cole Geerts <cjgeerts@gmail.com>
2022-08-18 16:04:26 -0400

    Save and Edit Fixes

--- wp-content/plugins/sandwich-selector/src/edit.js
+++ wp-content/plugins/sandwich-selector/src/edit.js
@@ -1,18 +1,9 @@
 import { __ } from '@wordpress/i18n';
 import { useBlockProps } from '@wordpress/block-editor';
-import { useState } from '@wordpress/element';
 import './editor.scss';
 
 export default function Edit({ attributes, setAttributes }) {
-	// const { title } = attributes;
-
-	// const [nodeTitle, setNodeTitle] = useState('');
-
-	// console.log(`Title: ${title}`);
-
 	const updateAttribute = (e) => {
-		console.log(e.target.value);
-
 		setAttributes({ title: e.target.value });
 	}
 
@@ -20,10 +11,6 @@ export default function Edit({ attributes, setAttributes }) {
 		<div {...useBlockProps()}>
 			<p>Title:</p>
 			<input type="text" value={attributes.title} onChange={(e) => updateAttribute(e)} />
-			{__(
-				'Sandwich Selector – hello from the editor!',
-				'sandwich-selector'
-			)}
 		</div>
 	);
 }
